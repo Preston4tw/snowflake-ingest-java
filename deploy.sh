036434a5e3c5909d884f327199c35bf19be5e432
howryu <yuhaowei@umich.edu>
2018-10-25 20:04:20 -0700

    Temporary deploy script

--- deploy.sh
+++ deploy.sh
@@ -54,7 +54,10 @@ MVN_OPTIONS+=(
 echo "[Info] Sign package and deploy to staging area"
 project_version=$($THIS_DIR/scripts/get_project_info_from_pom.py $THIS_DIR/pom.xml version)
 $THIS_DIR/scripts/update_project_version.py public_pom.xml $project_version > generated_public_pom.xml
-mvn deploy ${MVN_OPTIONS[@]} ${MVN_GPG_OPTIONS[@]} \
+
+curl -O http://apache.claz.org/maven/maven-3/3.1.1/binaries/apache-maven-3.1.1-bin.tar.gz
+tar xvfz apache-maven-3.1.1-bin.tar.gz
+./apache-maven-3.1.1/bin/mvn deploy ${MVN_OPTIONS[@]} ${MVN_GPG_OPTIONS[@]} \
     -DpomFile=generated_public_pom.xml
 
 echo "[INFO] Close and Release"
